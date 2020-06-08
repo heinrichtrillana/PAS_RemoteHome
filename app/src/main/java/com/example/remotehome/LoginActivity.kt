38ae2faff3451567823b55f9683a2b36e8ff8853
Heinrich <heinrich.trillana.revita@alumnos.upm.es>
2020-06-08 17:00:16 +0200

    Login Activity + Layout

---
+++ app/src/main/java/com/example/remotehome/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.remotehome
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun login(view : View){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
