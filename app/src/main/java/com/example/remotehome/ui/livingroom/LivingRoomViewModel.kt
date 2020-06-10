f842b83533865f2cda8b611e2bade677a84c8163
Heinrich <heinrich.trillana.revita@alumnos.upm.es>
2020-06-10 21:36:57 +0200

    binded to view

--- app/src/main/java/com/example/remotehome/ui/livingroom/LivingRoomViewModel.kt
+++ app/src/main/java/com/example/remotehome/ui/livingroom/LivingRoomViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.remotehome.network.SensorAPI
-import com.example.remotehome.network.SensorProperty
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
